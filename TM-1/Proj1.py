468fc2af87ce6d07eca9050f47b581c410f4ecf5
Hardik Sakhuja <hardiksakhuja421997@gmail.com>
2020-04-18 01:28:54 +0530

    TM-1 mini project

---
+++ TM-1/Proj1.py
@@ -0,0 +1,7 @@
+miles = int(input("How far would you like to go in miles?"))
+if miles <= 3:
+    print("I suggest Bicycle to your destination")
+elif miles<=300:
+    print("I suggest Motor-cycle to your destination")
+else:
+    print("I suggest Super-Car to your destination")
---
+++ TM-1/Proj2.py
@@ -0,0 +1,9 @@
+perhour = 0.51
+print("Question 1 -- How much does it cost to operate one server per day?")
+print("Answer -- ",perhour*24,end="\n\n")
+print("Question 2 -- How much does it cost to operate one server per week?")
+print("Answer -- ",perhour*24*7,end="\n\n")
+print("Question 3 -- How much does it cost to operate one server per month?")
+print("Answer -- ",perhour*24*7*30,end="\n\n")
+print("Question 1 -- How much days can i operate one server with 918 ?")
+print("Answer -- ",918/(perhour*24),end="\n\n")
